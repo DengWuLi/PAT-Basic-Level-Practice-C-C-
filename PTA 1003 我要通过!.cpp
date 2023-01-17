4e10bd8d30cf0d7b8db75931b6fc950ed2aea6b7
DengWuLi <109462385+DengWuLi@users.noreply.github.com>
2023-01-17 21:29:37 +0800

    Update PTA 1003 我要通过!.cpp
    更新了一下, 写了更详尽的注释, 以及更改了部分变量的命名.

--- PTA 1003 我要通过!.cpp
+++ PTA 1003 我要通过!.cpp
@@ -1,60 +1,74 @@
-/* PTA 1003 我要通过!
- * 2022/07/17
- * */
+/// PTA 1003 我要通过!
+/// 首次写于: 2022-07-17, 修改与 2023-01-17
+/// m没想到竟然都在去年7月和今年1月碰到这个题, 真可以说是缘分的呢.
+/// 之前太注意通过了, 代码没怎么写注释, 今天回来不上, ☺
+
 #include <iostream>
+
 using namespace std;
 
-bool judge(string s)
-{
-	int a[5] = {0};
-	for (int i = 0; i < s.length(); i++)
-	{
-		if (s[i] != 'P' && s[i] != 'A' && s[i] != 'T')
-			return false;
-		if (s[i] == 'P')
-		{
-			a[0]++;
-			if (a[1] != 0 || a[0] > 1)
-				return false;
-		}
-		else if (s[i] == 'T')
-		{
-			a[1]++;
-			if (a[0] == 0 || a[1] > 1)
-				return false;
-		}
-		else
-		{
-			if (a[0] == 0 && a[1] == 0)
-				a[2]++;
-			else if (a[0] != 0 && a[1] == 0)
-				a[3]++;
-			else
-				a[4]++;
-		}
-	}
-	
-	if (a[0] == 0 || a[1] == 0 || a[3] == 0)
-		return false;
-	else if (a[3] * a[2] != a[4])
-		return false;
-	else
-		return true;
-	
+bool judge(string s) {
+    // 这两个变量分别用来记录字符串中P, T出现的次数
+    int PNum = 0, TNum = 0;
+
+    /**
+     * 用来统计A出现的次数, 根据题意可以将A出现的位置划分为三中情况:
+     * <ol>
+     * <li>出现在P的左边, 用<strong>ANum[0]</strong>表示</li>
+     * <li>出现在P和T的中间, 用<strong>ANum[1]</strong>表示</li>
+     * <li>出现在T的右边, 用<strong>ANum[3]</strong>表示</li>
+     * </ol>
+     * 统计到最后只要判断一下:
+     * <p>ANum[0]*ANum[1]?=ANum[3]</p>即可
+     * */
+
+    int ANum[3] = {0};
+    int a[5] = {0};
+    for (char i : s) {
+        // 当一个字符串中出现了非`P`, `A`, `T`这三种字符时, 直接返回false
+        if (i != 'P' && i != 'A' && i != 'T')
+            return false;
+        // 当检测到字符=='P'时, PNum++, 同时进行判断, 若字符串中P出现的次数>1 || 在P出现之前, T已经出现, 直接返回false
+        if (i == 'P') {
+            PNum++;
+            if (PNum > 1 || TNum != 0)
+                return false;
+        }
+        // 当检测到字符=='T'时, TNum++, 同时进行判断, 若字符串中T出现的次数>1 || 在T出现后, P仍未出现, 直接返回false.
+        else if (i == 'T') {
+            TNum++;
+            if (TNum > 1 || PNum == 0)
+                return false;
+        }
+        else {
+            if (PNum == 0 && TNum == 0)
+                ANum[0]++;
+            else if (PNum != 0 && TNum == 0)
+                ANum[1]++;
+            else
+                ANum[2]++;
+        }
+    }
+    // "PAAAAA" 这样的代码也是不能通过的
+    if (PNum == 0 || TNum == 0 || ANum[1] == 0)
+        return false;
+
+    if(ANum[0]*ANum[1]!=ANum[2])
+        return false;
+    else
+        return true;
 }
 
-int main()
-{
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		string s;
-		cin >> s;
-		if (judge(s))
-			cout << "YES" << endl;
-		else
-			cout << "NO" << endl;
-	}
-	return 0;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        if (judge(s))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
 }
