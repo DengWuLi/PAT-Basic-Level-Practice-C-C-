cdfba9529ef65c338ad629579d927fd7f4a36ed5
DengWuLi <109462385+DengWuLi@users.noreply.github.com>
2023-01-24 22:25:11 +0800

    Update PTA 1005 继续(3n+1)猜想 .cpp

--- PTA 1005 继续(3n+1)猜想 .cpp
+++ PTA 1005 继续(3n+1)猜想 .cpp
@@ -1,40 +1,38 @@
-/* PTA 1004 成绩排名
- * 2022/07/18
- * */
-#include<iostream>
+#include <algorithm>
+#include <iostream>
+
 using namespace std;
 
-struct student
-{
-  char name[11];
-  char number[11];
-  int score;
-};
+int main() {
+    int n;
+    cin >> n;
+    int p[n];
+    for (int i = 0; i < n; i++)
+        cin >> p[i];
+    for (int i = 0; i < n; i++) {
+        if (p[i] == 0)
+            continue;
+        else {
+            for (int j = p[i]; j != 1;) {
+                if (j % 2 == 0)
+                    j /= 2;
+                else
+                    j = (3 * j + 1) / 2;
+                for (int k = 0; k < n; k++)
+                    if (p[k] == j)
+                        p[k] = 0;
+            }
+        }
+    }
+    sort(p, p + n, greater<>());
+    int m = 0;
+    for (; m < n; m++)
+        if (p[m] == 0)
+            break;
 
-int main()
-{
-	int N;
-	cin >> N;
-	const int n = N;
-	student stu[n];
-	for (int i = 0; i < N; i++)
-	{
-		cin >> stu[i].name >> stu[i].number >> stu[i].score;
-	}
-	student temp;
-	for (int i = 1; i < N; i++)
-	{
-		for (int j = 0; j <= N - 1 - i; j++)
-		{
-			if (stu[j].score > stu[j + 1].score)
-			{
-				temp = stu[j];
-				stu[j] = stu[j + 1];
-				stu[j + 1] = temp;
-			}
-		}
-	}
-	cout << stu[n - 1].name << " " << stu[n - 1].number << endl;
-	cout << stu[0].name << " " << stu[0].number;
-	
+    for (int i = 0; i < m; i++) {
+        cout << p[i];
+        if (i != m - 1)
+            cout << " ";
+    }
 }
